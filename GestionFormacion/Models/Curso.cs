1072435b7b6cac492f2b2b79c54434e43f274f58
pompone <54503871+pompone@users.noreply.github.com>
2026-05-24 10:04:46 -0300

    cuarto Comint

---
+++ GestionFormacion/Models/Curso.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionFormacion.Models
+{
+    public class Curso
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NombreCurso { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime FechaFin { get; set; }
+
+        public int Horas { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
