f1c2bb2f8ea04adeea623f8407d168c90c7f66c4
pompone <54503871+pompone@users.noreply.github.com>
2026-05-25 07:45:38 -0300

    Add files via upload

---
+++ GestionFormacion/Models/Inscripcion.cs
@@ -0,0 +1,17 @@
+﻿namespace GestionFormacion.Models
+{
+    public class Inscripcion
+    {
+        public int Id { get; set; }
+
+        public int EmpleadoId { get; set; }
+        public Empleado Empleado { get; set; }
+
+        public int CursoId { get; set; }
+        public Curso Curso { get; set; }
+
+        public DateTime FechaInscripcion { get; set; } = DateTime.Now;
+
+        public string Estado { get; set; } = "Pendiente";
+    }
+}
