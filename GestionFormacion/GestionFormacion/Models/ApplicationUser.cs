a23f2d501aa46c6355a8d5ad5dfb69678f3c7c33
pompone <54503871+pompone@users.noreply.github.com>
2026-05-25 07:36:16 -0300

    Add files via upload

---
+++ GestionFormacion/GestionFormacion/Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace GestionFormacion.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public bool DebeCambiarPassword { get; set; } = false;
+
+        public bool SolicitoResetPassword { get; set; } = false;
+
+        public DateTime? FechaSolicitudReset { get; set; }
+    }
+}
