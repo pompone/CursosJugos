60cf373467cdc42bb77aaf72f163ebbc0c30236b
pompone <54503871+pompone@users.noreply.github.com>
2026-05-25 07:42:35 -0300

    Add files via upload

---
+++ Models/Empleado.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionFormacion.Models
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Legajo { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellido { get; set; }
+
+        [Required]
+        public string Dni { get; set; }
+
+        public string Sector { get; set; }
+
+        public string Puesto { get; set; }
+
+        public bool Activo { get; set; } = true;
+    }
+}
